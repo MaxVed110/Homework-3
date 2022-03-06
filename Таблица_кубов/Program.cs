797735dfaf3ebb2bf6f0822f87567b18a29b3c4a
Maxim <vedernikovmaxim@rambler.ru>
2022-03-06 23:01:37 +0300

    Создана программа подсчёта кубов

---
+++ Таблица_кубов/Program.cs
@@ -0,0 +1,14 @@
+﻿int number = new int();
+double cube = new double();
+
+
+Console.WriteLine("Введите число: ");
+number = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Таблица кубов: ");
+for (int count = 1; count < number; count++)
+{
+    cube = Math.Pow(count, 3);
+    Console.Write($"{cube}, ");
+}
+Console.Write($"{Math.Pow(number, 3)}.");
