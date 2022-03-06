dff93b29da68c0593b1adb0d9c2667cebe55f560
Maxim <vedernikovmaxim@rambler.ru>
2022-03-06 22:46:05 +0300

    Создана программа нахождения расстояния между 2 точками

---
+++ Расстояние_между_точками_в_3D/Program.cs
@@ -0,0 +1,22 @@
+﻿double xA = new double();
+double yA = new double();
+double zA = new double();
+double xB = new double();
+double yB = new double();
+double zB = new double();
+double distance = new double();
+
+Console.WriteLine("Введите координаты первой точки: ");
+xA= Convert.ToDouble(Console.ReadLine());
+yA= Convert.ToDouble(Console.ReadLine());
+zA= Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Введите координаты второй точки: ");
+xB= Convert.ToDouble(Console.ReadLine());
+yB= Convert.ToDouble(Console.ReadLine());
+zB= Convert.ToDouble(Console.ReadLine());
+
+distance = Math.Pow(((xB-xA)*(xB-xA) + (yB-yA)*(yB-yA) + (zB-zA)*(zB-zA)), 0.5);
+distance = Math.Round(distance, 2);
+
+Console.WriteLine($"Расстояние между точками: {distance}");
